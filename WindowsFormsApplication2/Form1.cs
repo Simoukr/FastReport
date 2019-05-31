277888ee58dd8e324f4c4ccf2ef4609a579c44d2
1721808005 <1721808005@qq.com>
2019-05-31 09:53:26 +0800

    我的水晶报表

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -27,7 +27,7 @@ namespace WindowsFormsApplication2
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void button1_Click(object sender, EventArgs e)
+         private void button1_Click(object sender, EventArgs e)
         {
                 CrystalReport1 cr1 = new CrystalReport1();
                 cr1.SetDataSource(GetDataTable()); cr1.PrintOptions.PaperSize = PaperSize.PaperA4;
