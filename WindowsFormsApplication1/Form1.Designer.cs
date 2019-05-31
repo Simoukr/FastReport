c98a9c609b0a614a00f532f90546926125209611
1721808005 <1721808005@qq.com>
2019-05-31 09:52:20 +0800

    添加项目文件。

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.crystalReportViewer2 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.crystalReportViewer3 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.crystalReportViewer4 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.crystalReportViewer5 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.crystalReportViewer6 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.crystalReportViewer7 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.crystalReportViewer8 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.crystalReportViewer9 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.crystalReportViewer10 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(681, 875);
+            this.crystalReportViewer1.TabIndex = 0;
+            this.crystalReportViewer1.Load += new System.EventHandler(this.crystalReportViewer1_Load);
+            // 
+            // crystalReportViewer2
+            // 
+            this.crystalReportViewer2.ActiveViewIndex = -1;
+            this.crystalReportViewer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer2.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer2.Name = "crystalReportViewer2";
+            this.crystalReportViewer2.Size = new System.Drawing.Size(681, 875);
+            this.crystalReportViewer2.TabIndex = 1;
+            // 
+            // crystalReportViewer3
+            // 
+            this.crystalReportViewer3.ActiveViewIndex = -1;
+            this.crystalReportViewer3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer3.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer3.Name = "crystalReportViewer3";
+            this.crystalReportViewer3.Size = new System.Drawing.Size(681, 875);
+            this.crystalReportViewer3.TabIndex = 2;
+            // 
+            // crystalReportViewer4
+            // 
+            this.crystalReportViewer4.ActiveViewIndex = -1;
+            this.crystalReportViewer4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer4.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer4.Name = "crystalReportViewer4";
+            this.crystalReportViewer4.Size = new System.Drawing.Size(681, 875);
+            this.crystalReportViewer4.TabIndex = 3;
+            // 
+            // crystalReportViewer5
+            // 
+            this.crystalReportViewer5.ActiveViewIndex = -1;
+            this.crystalReportViewer5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer5.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer5.Name = "crystalReportViewer5";
+            this.crystalReportViewer5.Size = new System.Drawing.Size(681, 875);
+            this.crystalReportViewer5.TabIndex = 4;
+            // 
+            // crystalReportViewer6
+            // 
+            this.crystalReportViewer6.ActiveViewIndex = -1;
+            this.crystalReportViewer6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer6.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer6.Name = "crystalReportViewer6";
+            this.crystalReportViewer6.Size = new System.Drawing.Size(681, 875);
+            this.crystalReportViewer6.TabIndex = 5;
+            // 
+            // crystalReportViewer7
+            // 
+            this.crystalReportViewer7.ActiveViewIndex = -1;
+            this.crystalReportViewer7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer7.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer7.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer7.Name = "crystalReportViewer7";
+            this.crystalReportViewer7.Size = new System.Drawing.Size(681, 875);
+            this.crystalReportViewer7.TabIndex = 6;
+            // 
+            // crystalReportViewer8
+            // 
+            this.crystalReportViewer8.ActiveViewIndex = -1;
+            this.crystalReportViewer8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer8.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer8.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer8.Name = "crystalReportViewer8";
+            this.crystalReportViewer8.Size = new System.Drawing.Size(681, 875);
+            this.crystalReportViewer8.TabIndex = 7;
+            // 
+            // crystalReportViewer9
+            // 
+            this.crystalReportViewer9.ActiveViewIndex = -1;
+            this.crystalReportViewer9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer9.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer9.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer9.Name = "crystalReportViewer9";
+            this.crystalReportViewer9.Size = new System.Drawing.Size(681, 875);
+            this.crystalReportViewer9.TabIndex = 8;
+            // 
+            // crystalReportViewer10
+            // 
+            this.crystalReportViewer10.ActiveViewIndex = -1;
+            this.crystalReportViewer10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer10.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer10.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer10.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer10.Name = "crystalReportViewer10";
+            this.crystalReportViewer10.Size = new System.Drawing.Size(681, 875);
+            this.crystalReportViewer10.TabIndex = 9;
+            this.crystalReportViewer10.Load += new System.EventHandler(this.crystalReportViewer10_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 875);
+            this.Controls.Add(this.crystalReportViewer10);
+            this.Controls.Add(this.crystalReportViewer9);
+            this.Controls.Add(this.crystalReportViewer8);
+            this.Controls.Add(this.crystalReportViewer7);
+            this.Controls.Add(this.crystalReportViewer6);
+            this.Controls.Add(this.crystalReportViewer5);
+            this.Controls.Add(this.crystalReportViewer4);
+            this.Controls.Add(this.crystalReportViewer3);
+            this.Controls.Add(this.crystalReportViewer2);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer2;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer3;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer4;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer5;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer6;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer7;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer8;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer9;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer10;
+    }
+}
+
