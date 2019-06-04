a756e1c7b5eb5dbf05226be3ae22f668bb9e2872
1721808005 <1721808005@qq.com>
2019-06-04 14:46:54 +0800

    我的水晶报表打印

--- WindowsFormsApplication2/Program.cs
+++ WindowsFormsApplication2/Program.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApplication2
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
